f0d188a442e7dc58905c18fdbbdd52b73160b194
Edward Guevara <stormregalia@Edwards-MBP.lan>
2017-04-13 09:34:58 -0700

    better documentation

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -40,6 +40,7 @@ QueryResult::~QueryResult() {
 }
 QueryResult *SQLExec::execute(const hsql::SQLStatement *statement) throw(SQLExecError) {
     try {
+    //initialize SQLExec::tables if not yet
         if(SQLExec::tables==nullptr)
             SQLExec::tables = new Tables();
         switch (statement->type()) {
@@ -57,6 +58,7 @@ QueryResult *SQLExec::execute(const hsql::SQLStatement *statement) throw(SQLExec
     }
 }
 QueryResult *SQLExec::create(const hsql::CreateStatement *statement) {
+    //check if statement is for tables
     if(statement->type!= hsql::CreateStatement::kTable)
         return new QueryResult("All but Table Not Implemented");
     ValueDict* insertRow = new ValueDict();
@@ -65,9 +67,13 @@ QueryResult *SQLExec::create(const hsql::CreateStatement *statement) {
     ColumnNames newColNames;
     ColumnAttributes newColAttributes;
     ColumnAttribute colAttribute;
+
+    //get columns table
     Columns& columnTable = (Columns&)  SQLExec::tables->get_table(Columns::TABLE_NAME);
     std::string tableName = statement->tableName;
     (*insertRow)["table_name"] = tableName;
+
+    //insert table name into _tables
     tableHandle = SQLExec::tables->insert(insertRow);
     try {
         std::map<Identifier,Value>::iterator it;
@@ -84,23 +90,30 @@ QueryResult *SQLExec::create(const hsql::CreateStatement *statement) {
             }
             else
                 throw DbRelationError("Type Not Implemented");
+
+            //get all handles in _columns in case column insert fails
             colHandles.push_back(columnTable.insert(insertRow));
             newColAttributes.push_back(colAttribute);
         }
     } catch(DbRelationError& e) {
+        //delete added rows in _columns and _tables
         for(auto const& colHandle:colHandles)
             columnTable.del(colHandle);
         SQLExec::tables->del(tableHandle);
         throw(e);
     }
+    //create table
     HeapTable newTable(tableName,newColNames,newColAttributes);
     newTable.create();
     return new QueryResult("created " + tableName);
 }
 QueryResult *SQLExec::drop(const hsql::DropStatement *statement) {
+    //check if statement is table
     if(statement->type!= hsql::DropStatement::EntityType::kTable)
         return new QueryResult("All but Table Not Implemented");
     std::string name = statement->name;
+
+    //try to get table from _tables and drop it
     try{
         HeapTable& table = (HeapTable&) SQLExec::tables->get_table(statement->name);
         table.drop();
@@ -108,12 +121,16 @@ QueryResult *SQLExec::drop(const hsql::DropStatement *statement) {
     {
         throw(e);
     }
+
+    //delete from _tables
     HeapTable& tablesTable = (HeapTable&) SQLExec::tables->get_table(Tables::TABLE_NAME);
     ValueDict tableName;
     tableName["table_name"] = Value(statement->name);
     Handles delTableHandle = *tablesTable.select(&tableName);
     for(auto const& deleteTable : delTableHandle)
         tables->del((deleteTable));
+
+    //delete from _columns
     HeapTable& colTable = (HeapTable&) SQLExec::tables->get_table(Columns::TABLE_NAME);
     Handles delColHandles = *colTable.select(&tableName);
     for(auto const& deleteColTable : delColHandles)
@@ -136,11 +153,13 @@ QueryResult *SQLExec::show_tables(const hsql::ShowStatement *statement) {
     ColumnNames *colNames = new ColumnNames();
     ColumnAttributes *colAttributes = new ColumnAttributes();
     ValueDicts* rows = new ValueDicts();
+
+    //get _tables table and the columns for _tables for display
     tables->get_columns(Tables::TABLE_NAME, *colNames, *colAttributes);
     HeapTable &tableNames = (HeapTable&)tables->get_table(Tables::TABLE_NAME);
 
+    //get all rows in _tables
     Handles* handles = tableNames.select();
-
     for(auto const& handle: *handles) {
         ValueDict* valHandle = tableNames.project(handle);
         Value val = (*valHandle)["table_name"];
@@ -155,10 +174,12 @@ QueryResult *SQLExec::show_columns(const hsql::ShowStatement *statement) {
     ColumnNames *colNames = new ColumnNames();
     ColumnAttributes *colAttributes = new ColumnAttributes();
     ValueDicts* rows = new ValueDicts();
+
+    //get _columns table and the columns for _columns for display
     HeapTable &colTable = (HeapTable&)tables->get_table(Columns::TABLE_NAME);
     tables->get_columns(Columns::TABLE_NAME, *colNames, *colAttributes);
-    if(colNames->size()==0)
-        return new QueryResult("Table not found");
+
+    //get columns based on given table name
     Handles* handles = colTable.select();
     for(auto const& handle: *handles) {
         ValueDict* valHandle = colTable.project(handle);
